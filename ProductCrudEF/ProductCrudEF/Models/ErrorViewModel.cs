f80c58d234eedeb6c2d826abc21ec48622abb700
brhmv <114811800+brhmv@users.noreply.github.com>
2024-01-23 23:09:18 +0400

    Delete ProductCrudEF directory

--- ProductCrudEF/ProductCrudEF/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProductCrudEF.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
